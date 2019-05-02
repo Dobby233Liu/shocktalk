049d89e8f0630f7022b36741cb1f3943e6c2174f
Liu-Wen-Yuan <15816141883@163.com>
2019-05-02 11:17:52 +0800

    。

--- public/assets/js/talkwindow-basic.js
+++ public/assets/js/talkwindow-basic.js
@@ -2,11 +2,11 @@ window.lastLine = "";
 function refresh(){
 	jQuery.get("./safe_getlasttalked", function(resp){
 		if (window.lastLine != resp) {
+			if (window.lastLine != "") document.getElementById("nmsg").play();
 			window.lastLine = resp;
 			var _p = document.createElement("p");
 			_p.innerHTML = window.lastLine;
 			document.body.appendChild(_p);
-			document.getElementById("nmsg").play();
 		}
 	});
 }
--- src/com/github/dobby233liu/shocktalk/Main.js
+++ src/com/github/dobby233liu/shocktalk/Main.js
@@ -21,38 +21,43 @@ function readFileToArr(fReadName,callback){
 }
 
 function parse (str) {
-      // str假如为某个DOM字符串
-      // 1. result为处理之后的DOM节点
-      let result = ''
-      // 2. 解码
-      let decode = he.unescape(str, {
-          strict: true
-      })
-      HTMLParser(decode, {
-          start (tag, attrs, unary) {
-              // 3. 过滤常见危险的标签
-              if (tag === 'script' || tag === 'link' || tag === 'style' || tag === 'iframe' || tag === 'frame') return
-              result += "<" + tag
-              for (let i = 0; i < attrs.length; i++) {
-                  let name = (attrs[i].name).toLowerCase()
-                  let value = attrs[i].escaped
-                  // 3. 过滤掉危险的style属性和js事件
-                  if (name === 'style' || name === 'href' || name === 'src' || ~name.indexOf('on')) continue
-                  result += ` ${name}=${value}`
-              }
-              result += `${unary ? ' /' : ''} >`
-          },
-          chars (text) {
-              result += text
-          },
-          comment (text) {
-              result += `<!-- ${text} -->`
-          },
-          end (tag) {
-              result += `</${tag}>`
-          }
-      })
-      return result
+	try{
+		  // str假如为某个DOM字符串
+		  // 1. result为处理之后的DOM节点
+		  let result = ''
+		  // 2. 解码
+		  let decode = he.unescape(str, {
+			  strict: true
+		  })
+		  HTMLParser(decode, {
+			  start (tag, attrs, unary) {
+				  // 3. 过滤常见危险的标签
+				  if (tag === 'script' || tag === 'link' || tag === 'style' || tag === 'iframe' || tag === 'frame') return
+				  result += "<" + tag
+				  for (let i = 0; i < attrs.length; i++) {
+					  let name = (attrs[i].name).toLowerCase()
+					  let value = attrs[i].escaped
+					  // 3. 过滤掉危险的style属性和js事件
+					  if (name === 'href' || ~name.indexOf('on')) continue
+					  result += ` ${name}=${value}`
+				  }
+				  result += `${unary ? ' /' : ''} >`
+			  },
+			  chars (text) {
+				  result += text
+			  },
+			  comment (text) {
+				  result += `<!-- ${text} -->`
+			  },
+			  end (tag) {
+				  result += `</${tag}>`
+			  }
+		  });
+		  return result
+	}catch(e){
+		console.error(e);
+		return str;
+	}
 }
 
 module.exports.main = function(argv, moduleDirectory){
@@ -70,7 +75,7 @@ module.exports.main = function(argv, moduleDirectory){
 	});
 	app.get("/safe_getlasttalked", function(req, res, next){
 		readFileToArr(moduleDirectory + "/data/talked.txt", function(array){
-			res.send(array[array.length - 1]);
+			res.send(parse(array[array.length - 1]));
 		});
 	});
 	app.listen(argv[0], () => console.log("Shocktalk started on port " + argv[0] + "!"))
